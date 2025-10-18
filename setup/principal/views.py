82d365a3a229b691809e5ed706b890edf0d95dd9
matheus silva <matheusminiblindao@gmail.com>
2025-10-17 21:05:01 -0300

    removendo midlere de bloqueio

--- setup/principal/views.py
+++ setup/principal/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-# @login_required
+@login_required
 def home(request):
     return render(request, 'principal/home.html')
